49ac748396826d452360f323c4dbf47520d4414c
Kalitin Andrey <17819dron@gmail.com>
2017-07-22 16:55:55 +0200

    добавлена нумерация страниц

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('promise-mysql');
+var Promise = require("bluebird");
 
 var app = express();
 // подключаем бодипарсер, который разбирает тело запроса
@@ -29,86 +30,105 @@ client.query('SELECT pass FROM users WHERE login = \`тта\`', function(error, 
 */
 app.get('/page/:page', function(req, res) {
 	let count = 'SELECT COUNT(*) as total FROM comments';
-	client.query(count)
-		.then(([{total: tableLength}]) => {
-			let maxPage = Math.round(tableLength / 10);
+	client.query(count).then(([{total: tableLength}]) => {
+		let maxPage = Math.ceil(tableLength / 10);
 
-			let start = (req.params.page - 1) * 10;
-			if(req.params.page > maxPage) {
-				start = maxPage * 10;
-			} else if(req.params.page < 1){
-				start = 0;
-			}
+		let currentPage = req.params.page;
 
-			let sql = mysql.format('SELECT name, comment, id FROM comments LIMIT 10 OFFSET ?', [start]);
-			return client.query(sql);
-		})
-		.then((comments) => {
-			let commentsHTML = '';
-			for(let i = 0; i < comments.length; i++) {
-				commentsHTML += `
-					<p class="name">
-						${comments[i].name}
-					</p>
-					<p class="text">
-						${comments[i].id}
-					</p>
-					<p class="text">
-						${comments[i].comment}
-					</p>
+		let start = (currentPage - 1) * 10;
+		if(currentPage > maxPage) {
+			start = maxPage * 10;
+		} else if(currentPage < 1){
+			start = 0;
+		}
+		let pagesHTML = '';
+		for(let i = 1; i <= maxPage; i++) {
+			if(currentPage == i) {
+				pagesHTML += `
+					${i}
+				`;
+			} else {
+				pagesHTML += `
+					<a href="/page/${i}">${i}</a>
 				`;
 			}
-			res.send(`
-				<html>
-					<head>
-						<title>Комментирование на NODE.js</title>
-						<meta charset="utf-8">
-						<link href="/css/bootstrap.min.css" rel="stylesheet">
-						<link href="/main.css" rel="stylesheet">
-					</head>
-					<body>
-						<div class="container">
-							<div class="registration">
-								<h2>Регистрация</h2>
-								<form action="/reg" method="post">
-									<p>
-									<label>Ваш логин:<br></label>
-									<input name="user[name]" type="text" size="32" maxlength="32">
-									</p>
-									<p>
-									<label>Ваш пароль:<br></label>
-									<input name="user[password]" type="pass" size="32" maxlength="32">
-									</p>
-									<p>
-									<input type="email" name="user[email]" class="form-control" id="exampleInputEmail1" placeholder="jane.doe@example.com">
-									</p>
-									<p>
-									<input type="submit" name="submit" value="Зарегистрироваться/войти">
-									</p>
-								</form>
-							</div>
-							<div class="comments">
-								${commentsHTML}
-							</div>
+		}
 
-							<form class="form-inline" method="POST" action="/post.php">
-								<div class="form-group">
-									<label for="exampleInputName2">Name</label>
-									<input type="text" name="name" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
-								</div>
-								<div class="form-group">
-									<label for="exampleInputEmail2">Email</label>
-									<input type="email" name="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
-								</div>
-								<button type="submit" class="btn btn-default">Отправить сообщение</button></br>
-								<p><a href="exit.php">выход</a></p>
-								<textarea class="text_input" name="comment" rows="3"></textarea>
+		let sql = mysql.format('SELECT name, comment, id FROM comments LIMIT 10 OFFSET ?', [start]);
+
+		return Promise.props({
+			comments: client.query(sql),
+			pagesHTML
+		});
+	}).then(({pagesHTML, comments}) => {
+		let commentsHTML = '';
+		for(let i = 0; i < comments.length; i++) {
+			commentsHTML += `
+				<p class="name">
+					${comments[i].name}
+				</p>
+				<p class="text">
+					${comments[i].id}
+				</p>
+				<p class="text">
+					${comments[i].comment}
+				</p>
+			`;
+		}
+		res.send(`
+			<html>
+				<head>
+					<title>Комментирование на NODE.js</title>
+					<meta charset="utf-8">
+					<link href="/css/bootstrap.min.css" rel="stylesheet">
+					<link href="/main.css" rel="stylesheet">
+				</head>
+				<body>
+					<div class="container">
+						<div class="registration">
+							<h2>Регистрация</h2>
+							<form action="/reg" method="post">
+								<p>
+								<label>Ваш логин:<br></label>
+								<input name="user[name]" type="text" size="32" maxlength="32">
+								</p>
+								<p>
+								<label>Ваш пароль:<br></label>
+								<input name="user[password]" type="pass" size="32" maxlength="32">
+								</p>
+								<p>
+								<input type="email" name="user[email]" class="form-control" id="exampleInputEmail1" placeholder="jane.doe@example.com">
+								</p>
+								<p>
+								<input type="submit" name="submit" value="Зарегистрироваться/войти">
+								</p>
 							</form>
 						</div>
-					</body>
-				</html>
-			`);
-		})
+						<div class="comments">
+							${commentsHTML}
+						</div>
+						<div class="pages">
+							${pagesHTML}
+						</div>
+
+						<form class="form-inline" method="POST" action="/post.php">
+							<div class="form-group">
+								<label for="exampleInputName2">Name</label>
+								<input type="text" name="name" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+							</div>
+							<div class="form-group">
+								<label for="exampleInputEmail2">Email</label>
+								<input type="email" name="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+							</div>
+							<button type="submit" class="btn btn-default">Отправить сообщение</button></br>
+							<p><a href="exit.php">выход</a></p>
+							<textarea class="text_input" name="comment" rows="3"></textarea>
+						</form>
+					</div>
+				</body>
+			</html>
+		`);
+	})
 });
 
 app.get('/', function(req, res) {
