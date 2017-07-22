d75f8bead732bf784629c05edbe1f0bc842a3ef6
Kalitin Andrey <17819dron@gmail.com>
2017-07-22 15:05:11 +0200

    добавлен вывод комментариев из базы

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
 var app = express();
-// предоставляем к загрузке все файлы в каталоге client
-app.use(express.static('client'));
-// при загрузке страницы возвращаем файл стартовой страницы
-app.get('/', function(req, res){
-    res.sendFile( __dirname + '/client/index.html');
-});
 // подключаем бодипарсер, который разбирает тело запроса
 app.use(bodyParser.urlencoded({
 	extended: true
@@ -29,16 +23,80 @@ var client = mysql.createConnection({
 client.query('SELECT pass FROM users WHERE login = \`тта\`', function(error, result, fields){
 	console.log(JSON.stringify(result));
 });
-*/
-/*
-app.post('/', function(req, res) {
-	let sql = 'SELECT name, comment FROM comments';
-	client.query(sql, function(error, result, fields){
-		for(let i = 0; i <= result.length; i++) {
-			
-		}
-	});
+*//*
+app.get('/page/:id', function(req, res) {
+	let id = req.params.id;
+	console.log(id);
 });*/
+app.get('/', function(req, res) {
+	let sql = 'SELECT name, comment, id FROM comments LIMIT 10';
+	client.query(sql, function(error, result, fields) {
+		let comments = '';
+		for(let i = 0; i < result.length ; i++) {
+			comments += `
+				<p class="name">
+					${result[i].name}
+				</p>
+				<p class="text">
+					${result[i].id}
+				</p>
+				<p class="text">
+					${result[i].comment}
+				</p>
+			`;
+		}
+		res.send(`
+			<html>
+				<head>
+					<title>Комментирование на NODE.js</title>
+					<meta charset="utf-8">
+					<link href="/css/bootstrap.min.css" rel="stylesheet">
+					<link href="/main.css" rel="stylesheet">
+				</head>
+				<body>
+					<div class="container">
+						<div class="registration">
+							<h2>Регистрация</h2>
+							<form action="/reg" method="post">
+								<p>
+								<label>Ваш логин:<br></label>
+								<input name="user[name]" type="text" size="32" maxlength="32">
+								</p>
+								<p>
+								<label>Ваш пароль:<br></label>
+								<input name="user[password]" type="pass" size="32" maxlength="32">
+								</p>
+								<p>
+								<input type="email" name="user[email]" class="form-control" id="exampleInputEmail1" placeholder="jane.doe@example.com">
+								</p>
+								<p>
+								<input type="submit" name="submit" value="Зарегистрироваться/войти">
+								</p>
+							</form>
+						</div>
+						<div class="comments">
+							${comments}
+						</div>
+
+						<form class="form-inline" method="POST" action="/post.php">
+							<div class="form-group">
+								<label for="exampleInputName2">Name</label>
+								<input type="text" name="name" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+							</div>
+							<div class="form-group">
+								<label for="exampleInputEmail2">Email</label>
+								<input type="email" name="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+							</div>
+							<button type="submit" class="btn btn-default">Отправить сообщение</button></br>
+							<p><a href="exit.php">выход</a></p>
+							<textarea class="text_input" name="comment" rows="3"></textarea>
+						</form>
+					</div>
+				</body>
+			</html>
+		`);
+	});
+});
 // при получении POST запроса вставляем данные в таблицу через placeholder
 app.post('/reg', function(request, response){
 	let sql = mysql.format('SELECT login FROM users WHERE login = ?', [request.body.user.name]);
@@ -72,3 +130,6 @@ app.post('/reg', function(request, response){
 	// отправляем файл стартовой страницы
 	response.sendFile( __dirname + '/client/index.html');
 });
+
+// предоставляем к загрузке все файлы в каталоге client
+app.use(express.static('client'));
